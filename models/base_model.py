ee3d19f2191ea5aee237e418e0dd7c3e863e3820
elvis wangari <elviswangare@gmail.com>
2023-02-10 07:52:05 -0800

    Add: README File
    > AUTHORS File
    > Created BaseModel class with attribute and methods

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+parent class that will inherit
+"""
+from uuid import uuid4()
+from datetime import datetime
+
+
+class BaseModel:
+    """Represents the base model of the AIRBNB """
+
+    def __init__(self, *args, **kwargs):
+    """initialize a new basemodel
+    Args:
+    *args
+    **kwargs
+    """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns classname id and dict attribute"""
+        classname = self.__class__.__name__
+        return "[{}] ({}) {} ".format(classname, self.id, seld.__dict__)
+
+    def save(self):
+        """update updated_at to current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
+        r_dict = self.__dict__
+        r_dict["create_at"] = self.created_at.isoformat()
+        r_dict["updated_at"] = self.updated_at.isoformat()
+        r_dict["__class__"] = self.__class__.__name__
+
+        return r_dict
